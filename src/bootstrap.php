3721098c324c813f7b635a3105aa29d249e32f8b
Matt Smith <hellboy1975@gmail.com>
2015-12-16 12:29:17 +1030

    Move views folder into src

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-// require 'autoload.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
 require __DIR__.'/config.php';
@@ -18,7 +17,7 @@ $app->register(new Silex\Provider\ValidatorServiceProvider());
 
 // register providers
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path'       	=> __DIR__.'/../views',
+    'twig.path'       	=> __DIR__.'/../src/views',
     'twig.class_path' 	=> __DIR__.'/../vendor/twig/lib',
     'twig.cache'		=> (!DEBUG_APP),  // disble caching when debugging
 ));
